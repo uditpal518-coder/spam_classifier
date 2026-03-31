dc2ba31065161b4a62cfdd3ca5259669cf9adc8f
uditpal518-coder <uditpal518@gmail.com>
2026-03-31 21:32:10 +0530

    Add files via upload

---
+++ Spam_Project.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import joblib
+import re
+import pandas as pd
+import numpy as np
+
+st.set_page_config(layout='wide', page_title=" Spam Message Classifier", page_icon="🚫")
+
+
+st.markdown("""
+    <style>
+            
+    .hero-section {
+        background-image: url("https://www.shutterstock.com/image-vector/cybersecurity-warning-spam-sms-mobile-260nw-2714250287.jpg");
+        background-size: cover;
+        background-position: center;
+        padding: clamp(30px, 8vw, 80px);
+        border-radius: 20px;
+        text-align: center;
+        color: white;
+        box-shadow: 0 10px 30px rgba(0,0,0,0.2);
+        margin-bottom: 2rem;
+        width: 100%;
+    }
+
+    .main-heading {
+        font-size: clamp(2rem, 10vw, 4.5rem);
+        font-weight: 800;
+        margin: 0;
+        letter-spacing: 1px;
+        text-shadow: 2px 2px 15px rgba(0,0,0,0.5);
+    }
+    
+    .sub-text {
+        font-size: clamp(1rem, 3vw, 1.5rem);
+        opacity: 0.9;
+        margin-top: 10px;
+    }
+
+    </style>
+    
+    <div class="hero-section">
+        <h1 class="main-heading">🚫 Spam Message Classifier </h1>
+        <p class="sub-text">AI-Powered Spam Massage Classifier System</p>
+    </div>
+    """, unsafe_allow_html=True)
+    
+
+def mycleaning(doc):
+    return re.sub("[^a-zA-Z ]","",doc).lower()
+    
+model=joblib.load("Spam_model.pkl")
+
+
+with st.sidebar:
+    st.link_button("Food Sentiment Analysis","https://sentiment05.streamlit.app/")
+
+    st.divider()
+
+    st.image("spam.jpg")
+
+    st.divider()
+    
+    st.title("🚀 About Project")
+    st.info("This System uses Natural Language Processing (NLP) to automatically detect Spam Message.")
+    
+    st.title("🛠️ Technical Stack")
+    st.write("**Streamlit:** Framework for creating the interactive web interface.")
+    st.write("**Scikit-Learn:** Used for training and implementing the Sentiment Model.")
+    st.write("**Pandas:** For data manipulation and reading CSV files.")
+    st.write("**Numpy:** For mathematical operations and array processing.")
+    st.write("**Joblib:** To load the pre-trained Machine Learning model.")
+    st.write("**Regex (re):** For cleaning and preprocessing the text data.")
+    
+    st.divider()
+    
+    st.title("📞 Contact Us")
+    st.success("📍 **AI Engineers @ DUCAT**")
+    st.write("📧 **Email:** uditpal518@gmail.com")
+    st.write("📱 **Phone:** +91 99999-88888")
+    st.write("🌐 **Website:** www.ducatindia.com")
+
+
+st.write("\n")
+st.write("#### Enter Msg")
+sample=st.text_input("")
+if st.button("Predict"):
+    pred=model.predict([sample])
+    prob=model.predict_proba([sample])
+    if pred[0]=='ham':
+        st.success("Valid 👍")
+        st.write(f"Confidence Score : {prob[0][0]:.2f}")
+        st.balloons()
+    else:
+        st.error("SPAM 👎") 
+        st.write(f"Confidence Score : {prob[0][1]:.2f}")
+        st.balloons()
+
+st.divider()
+
+st.write("#### Bulk Prediction")
+file=st.file_uploader("select file",type=["csv","txt"])
+if file:
+    df=pd.read_csv(file,names=["MSG"])
+    placeholder=st.empty()
+    placeholder.dataframe(df)
+    if st.button("Predict",key="b2"):
+        corpus=df.MSG
+        pred=model.predict(corpus)
+        prob=np.max(model.predict_proba(corpus),axis=1)
+        df['Msg Type']=pred
+        df['Confidance']=prob
+        placeholder.dataframe(df)
